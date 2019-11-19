d0ec126ee53839d6ee6041bcdb70c7b03f01ed21
suffiang96 <56616975+suffiang96@users.noreply.github.com>
2019-11-18 19:09:36 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,52 @@
+////adding map
+var map = L.map('map').setView([47.615428, -122.334145], 11);
+
+//adding tiles
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 100,
+    id: 'mapbox.streets',
+    accessToken:'pk.eyJ1Ijoic3VmZmlhbmc5NiIsImEiOiJjazJqb3V4cXAwbXN1M2N0cmNlMDB0ODdyIn0.bSVgjiBLodS46nfCU1cKMw'
+}).addTo(map);
+
+//load GeoJSON
+$.getJSON("Faults.zip.geojson",function(faultData){
+  L.geoJson (faultData, {
+ style: function(feature){
+   return { color: 'black',
+          dashArray: '6',
+          weight: 2,
+          fillOpacity: 0.7 };
+ },
+ onEachFeature: function( feature, layer ){
+   layer.bindPopup( feature.properties.slip_sense + " type fault" )
+ }
+  }).addTo(map);
+});
+
+$.getJSON("hazZones.geojson",function(shakeData){
+L.geoJson(shakeData, {
+  style: function(feature){
+    var fillColor,
+      percent = feature.properties.PERCENT_G;
+    if ( percent = 110-145 ) fillColor = '#800026';
+    else if( percent = 80-110 ) fillColor = '#BD0026';
+    else if ( percent = 60-80 ) fillColor = '#E31A1C';
+    else if ( percent = 50-60 ) fillColor = '#FC4E2A';
+    else if ( percent = 40-50 ) fillColor = '#FD8D3C';
+    else if ( percent = 30-40 ) fillColor = '#FEB24C';
+    else if ( percent = 20-30 ) fillColor = '#FED976';
+    else fillColor = '#FFFFFF';
+    return {color: "#999", weight: 1, fillColor: fillColor, fillOpacity: .6 };
+    },
+
+      onEachFeature: function( feature, layer ){
+        layer.bindPopup( "<strong>" + feature.properties.PERCENT_G + "</strong><br/>" + feature.properties.PERCENT_G + "% shaking hazard" )
+      }
+    }).addTo(map);
+  });
+
+
+
+//L.geoJson(faultData).addTo(map);
+//});
